908744ab2383ee175473f478b522d1d116b1e52f
David D <i_robot73@hotmail.com>
2024-02-17 11:39:03 -0500

    Update my_backup.sh
    Updated for use across multiple servers

--- Backup/my_backup.sh
+++ Backup/my_backup.sh
@@ -1,26 +1,56 @@
 #!/bin/sh
 
 BACKUP_PATH=/data/backup
-BACKUP_INCLUDE_FILE=/data/bkup_include.txt
 NUM_DAYS_KEEP=3
+FILE_TYPE="D" # Use '(D)AY' or '(T)IMESTAMP'
+BACKUP="" # Do not edit unless you know what you are doing
 
-echo
-echo "Starting backup..."
+# EG: lgsm_(Monday|Tuesday|...|Sunday).tar.gz
+UseDayNamesForFiles() {
+  local backupfilename=lgsm_$(date -d "today" +"%A").tar.gz
+  BACKUP="${BACKUP_PATH}/${backupfilename}"
+}
 
-## Purge ( > # days ) Only run REMOVE if there are any files found
+# EG: lgsm_20240201_062111.tar.gz
+UseTimestampForFiles() {
+  local backupfilename=lgsm_$(date +%Y%m%d_%H%M%S).tar.gz
+  BACKUP="${BACKUP_PATH}/${backupfilename}"
+}
 
-find "$BACKUP_PATH" -name "*.tar.gz" -type f -mtime +$NUM_DAYS_KEEP | xargs rm -v -r
+IsFileTypeDay() {
+    local pat="^(d|D)"
+    if [[ "$FILE_TYPE" =~ $pat ]]; then
+        return 0 # true
+    else 
+        return 1 # false
+    fi
+}
 
-## Setup ##
+CleanUp() {
+  if IsFileTypeDay; then 
+    if [ -f "${BACKUP}" ]; then # Remove existing DAY file
+      echo "Existing backup detected.  Removing."
+      rm -v "${BACKUP}"
+    fi
+  else # Remove older TimeStamp based backups
+    find "$BACKUP_PATH" -name "*.tar.gz" -type f -mtime +$NUM_DAYS_KEEP | xargs rm -v -r
+  fi
+}
 
-FILENAME=lgsm_$(date +%Y%m%d_%H%M%S).tar.gz # lgsm_20240201_062111.tar.gz
-#FILENAME=lgsm_$(date -d "today" +"%A").tar.gz # lgsm_(Monday|Tuesday|...|Sunday).tar.gz
-BACKUP="${BACKUP_PATH}/${FILENAME}"
+Setup() {
+  if IsFileTypeDay; then
+    UseDayNamesForFiles
+  else
+    UseTimestampForFiles
+  fi
+}
 
-## Backup ##
+Setup
+CleanUp
 
-tar -czf "${BACKUP}" -C / -T "${BACKUP_INCLUDE_FILE}
+echo "Creating backup: '${BACKUP}'"
 
+tar -czf "${BACKUP}" -C / -X /data/bkup_exclude.txt -T /data/bkup_include.txt
 exitcode=$?
 
-echo "Completed: '${BACKUP}', Exit Code: ${exitcode}, Size $(du -sh "${BACKUP}" | awk '{print $1}')"
+echo "Finished.  Exit Code: ${exitcode}, Size $(du -sh "${BACKUP}" | awk '{print $1}')"
