3196fdcf7c41d70a855687ca0ad2b9e69b3f23f5
David D <i_robot73@hotmail.com>
2024-02-19 15:55:34 -0500

    Update my_backup.sh
    Added DEFAULTS if (EX|IN)CLUDE files are missing/empty

--- Backup/my_backup.sh
+++ Backup/my_backup.sh
@@ -3,16 +3,20 @@
 #set -x # Enable to DEBUG
 
 BACKUP_PATH=/data/backup
-FILE_TYPE="D" # Use '(D)AY' or '(T)IMESTAMP'
+FILE_TYPE="T" # Use '(D)AY' or '(T)IMESTAMP'
 NUM_DAYS_KEEP=3 # Only used with TIMESTAMP backups
 EXCLUDE_FILE="/data/bkup_exclude.txt"
 INCLUDE_FILE="/data/bkup_include.txt"
 # Do not edit unless you know what you are doing
 BACKUP=""
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+# Defaults if/when the EXCLUDE file is empty/missing
+EXCLUDE_PARAM_DEFAULTS="--exclude=backup --exclude=lgsm --exclude=log --exclude=steamapps --exclude='.*' --exclude='*.lock'"
+# Defaults if/when the INCLUDE file is empty/missing
+INCLUDE_PARAM_DEFAULTS="/data /app"
 
 IsFileTypeDay() {
-    local pat="^(d|D)"    
+    local pat="^(d|D)"
     if [[ "$FILE_TYPE" =~ $pat ]]; then
         return 0 # true
     else 
@@ -21,7 +25,7 @@ IsFileTypeDay() {
 }
 
 GenerateBackupFileName() {
-  local backupfilename  
+  local backupfilename
   if IsFileTypeDay; then
     backupfilename=lgsm_$(date -d "today" +"%A").tar.gz
   else
@@ -31,7 +35,7 @@ GenerateBackupFileName() {
 }
 
 CleanUp() {
-  if IsFileTypeDay; then 
+  if IsFileTypeDay; then
     if [ -f "${BACKUP}" ]; then # Remove existing DAY file
       echo "Existing backup detected.  Removing."
       rm -fv "${BACKUP}"
@@ -46,18 +50,25 @@ Backup() {
     exclude_param=""
     local include_param
     include_param=""
-    
-    if [ -f "$EXCLUDE_FILE" ]; then
+
+    # Check if the file(s) exist & have a file size
+    if [[ -f $EXCLUDE_FILE && -s $EXCLUDE_FILE ]]; then
         exclude_param="-X ${EXCLUDE_FILE}"
+    else
+        exclude_param=$EXCLUDE_PARAM_DEFAULTS
     fi
 
-    if [ -f "$INCLUDE_FILE" ]; then
+    #  If invalid, we'll make it so the backup doesn't start at the root level
+    if [[ -f $INCLUDE_FILE && -s $INCLUDE_FILE ]]; then
         include_param="-T ${INCLUDE_FILE}"
+    else
+        include_param=$INCLUDE_PARAM_DEFAULTS
     fi
 
-    tar -czf "${BACKUP}" -C / ${exclude_param} ${include_param}
-    
-    return $?
+
+    tar -czf "${BACKUP}" -C . --exclude-vcs --ignore-failed-read ${exclude_param} ${include_param} .
+
+    echo $?
 }
 
 # Gives some head read to more easily read in output (log+)
@@ -72,4 +83,12 @@ echo "Creating backup: '${BACKUP}'"
 exitcode=$(Backup)
 
 # Give final status
-echo "Finished.  Exit Code: ${exitcode}, Size $(du -sh "${BACKUP}" | awk '{print $1}')"
+if [[ $exitcode -eq 0 ]]; then
+    if [[ -n ${BACKUP} && -f ${BACKUP} ]]; then
+        echo "Finished.  Exit Code: ${exitcode}, Size $(du -sh "${BACKUP}" | awk '{print $1}')"
+    else
+        exit 99
+    fi
+else
+    echo "Backup failed.  Exit code: '$exitcode'"
+fi
