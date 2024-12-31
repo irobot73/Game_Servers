3003588402c2c312197a9d15a768e39ce6ba0a08
David D. <i_robot73@hotmail.com>
2024-12-31 16:19:38 -0500

    Update PW_BACKUP.bat
    Corrected extension for back-up file
    Added server's own BACKUP folder to the exclusion list (no stepping on toes)

--- Palworld/PW_BACKUP.bat
+++ Palworld/PW_BACKUP.bat
@@ -24,16 +24,16 @@ if %errorlevel% neq 0 (
 mkdir "%backupFolder%"
 
 :purge
-forfiles /P "%backupFolder%" /S /M *.zip /D -%numberBackupDaysToKeep% /C "cmd /c ECHO @path & DEL /Q @path"
+forfiles /P "%backupFolder%" /S /M *.RAR /D -%numberBackupDaysToKeep% /C "cmd /c ECHO @path & DEL /Q @path"
 
 REM Get timestamp for backup folder (date and time without seconds)
 for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value') do set "datetime=%%I"
 set "timestamp=!datetime:~0,4!_!datetime:~4,2!_!datetime:~6,2!-!datetime:~8,2!_!datetime:~10,2!_!datetime:~12,2!"
 
-set "backupName=%backupFolder%\Backup_%timestamp%.zip"
+set "backupName=%backupFolder%\Backup_%timestamp%.RAR"
 
-REM Create ZIP, exluding all but the CONFIG & SAVE folders/files
-"%winrarPath%" a -r -ep1 -m5 -x*\Crashes\* -x*\Crashes\ -x*\ImGui\* -x*\ImGui\ -x*\Logs\* -x*\Logs\ -x*\Config\CrashReportClient\* -x*\Config\CrashReportClient\ "!backupName!" ".\Pal\Saved\*"
+REM Create RAR, exluding all but the CONFIG & SAVE folders/files
+"%winrarPath%" a -r -ep1 -m5 -x*\Crashes\* -x*\Crashes\ -x*\ImGui\* -x*\ImGui\ -x*\Logs\* -x*\Logs\ -x*\Config\CrashReportClient\* -x*\Config\CrashReportClient\ -x*\SaveGames\0\*\backup\ "!backupName!" ".\Pal\Saved\*"
 
 IF EXIST "%backupName%" (
     ECHO Backup successful.
